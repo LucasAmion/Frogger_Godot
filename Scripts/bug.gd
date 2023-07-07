5d667408f124075f7b89b2deb600abbacbdffd0c
LucasAmion <38921799+LucasAmion@users.noreply.github.com>
2023-07-06 22:29:37 -0400

    bug behavior tree

--- Scripts/bug.gd
+++ Scripts/bug.gd
@@ -1,8 +1,10 @@
 extends CharacterBody2D
 
-@onready var frog = $"../Frog"
+var has_target = false
 var movement_speed: float = 50
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
+@onready var area_2d = $"../NavigationRegion2D/Area2D"
+@onready var frog = $"../Frog"
 
 @onready var sprite_2d = $Sprite2D
 @onready var animation_tree = $AnimationTree
@@ -26,9 +28,16 @@ func actor_setup():
 	# Now that the navigation map is no longer empty, set the movement target.
 	navigation_agent.target_position = frog.global_position
 
-func _physics_process(delta):
-	navigation_agent.target_position = frog.global_position
+func is_player_in_range():
+	return area_2d.overlaps_body(frog)
 
+func move_to_player():
+	has_target == false
+	navigation_agent.target_position = frog.global_position
+	
+	if navigation_agent.is_navigation_finished():
+		return BTTickResult.SUCCESS
+	
 	var current_agent_position: Vector2 = global_position
 	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
 
@@ -43,3 +52,52 @@ func _physics_process(delta):
 	elif velocity.x > 0:
 		sprite_2d.scale = Vector2(-1, 1)
 	move_and_slide()
+	return BTTickResult.RUNNING
+
+func sting():
+	state_machine.travel("sting")
+	return  BTTickResult.SUCCESS
+
+func move_randomly():
+	if has_target == false:
+		navigation_agent.target_position = global_position + Vector2(randi_range(-60, 60), randi_range(-60, 60))
+		has_target = true
+	
+	if navigation_agent.is_navigation_finished():
+		has_target = false
+		return BTTickResult.SUCCESS
+	
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * movement_speed
+
+	navigation_agent.set_velocity(new_velocity) 
+	velocity = new_velocity
+	if velocity.x < 0:
+		sprite_2d.scale = Vector2(1, 1)
+	elif velocity.x > 0:
+		sprite_2d.scale = Vector2(-1, 1)
+	move_and_slide()
+	return  BTTickResult.RUNNING
+
+
+#func _physics_process(delta):
+#	navigation_agent.target_position = frog.global_position
+#
+#	var current_agent_position: Vector2 = global_position
+#	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+#
+#	var new_velocity: Vector2 = next_path_position - current_agent_position
+#	new_velocity = new_velocity.normalized()
+#	new_velocity = new_velocity * movement_speed
+#
+#	navigation_agent.set_velocity(new_velocity) 
+#	velocity = new_velocity
+#	if velocity.x < 0:
+#		sprite_2d.scale = Vector2(1, 1)
+#	elif velocity.x > 0:
+#		sprite_2d.scale = Vector2(-1, 1)
+#	move_and_slide()
